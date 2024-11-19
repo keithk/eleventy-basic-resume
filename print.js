978c4eabffc6a74c8f9711e85066fc99d22b648c
keithkurson <keith@keithkurson.net>
2024-11-19 12:40:52 -0800

    🐛 Fix print.js localhost port

--- print.js
+++ print.js
@@ -3,7 +3,7 @@ import { chromium } from "playwright";
 async function printPDF() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto("http://localhost:1234/", { waitUntil: "networkidle" });
+  await page.goto("http://localhost:8080", { waitUntil: "networkidle" });
   await page.pdf({ format: "Letter", path: "./resume.pdf" });
   await browser.close();
 }
